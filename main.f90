c75f777cecc9744bb98e0120306f931ae696ec6d
Geo-prediction2025 <849769755@qq.com>
2025-09-03 15:55:43 +0800

    Add files via upload
    MC method of random_seed_number.

---
+++ main.f90
@@ -0,0 +1,13 @@
+    program main
+    implicit none 
+    integer , save :: flag = 0
+    double precision :: ran 
+    if(flag==0) then 
+      call random_seed()
+      flag = 1 
+    endif 
+    call random_number(ran)     ! built in fortran 90 random number function 
+     !!! returen number between 0 and 1
+    write(*,*) ran
+    
+    end program main
